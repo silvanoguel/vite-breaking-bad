dbedfd0dbbb7d163e8255afaa58b2b8430beb21d
silvanoguel <silvanoguel@msn.com>
2023-04-18 15:24:02 +0200

    add:searchbar

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,13 @@ import AppHeader from "./components/AppHeader.vue";
 import AlienList from "./components/AlienList.vue";
 import axios from "axios";
 import { store } from "./store";
+import SearchBar from "./components/SearchBar.vue";
 
 export default {
   components: {
     AppHeader,
-    AlienList
+    AlienList,
+    SearchBar
   },
   data() {
     return {
@@ -15,17 +17,36 @@ export default {
     }
   },
   mounted() {
-        store.loading = true;
-        axios.get(store.apiURL).then((resp) => {
-            this.store.aliens = resp.data.data;
-            store.loading = false;
-        })
+    this.getAliens();
+  },
+  methods: {
+    getAliens() {
+      this.store.loading = true;
+      const params = {};
+      if (this.store.selectedStatus) {
+        params.status = this.store.selectedStatus;
+      }
+      axios.get(this.store.apiURL, {
+        params
+      }).then(resp => {
+        this.store.aliens = resp.data.data;
+      }).catch(error => {
+        console.log(error);
+      }).finally(() => {
+        this.store.loading = false;
+      })
+    },
+    handleFilter() {
+      this.getAliens();
     }
+  }
+
 }
 </script>
 
 <template>
   <AppHeader title="Yu-Gi-Oh Api" />
+  <SearchBar @filter="handleFilter" />
   <AlienList />
 </template>
 
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,26 @@
+<script>
+import { store } from '../store';
+
+export default {
+    name: "SearchBar",
+    emits: ["filter"],
+    data() {
+        return {
+            store,
+            archetypeOptions: ["alien", "ally of justice", "ancient gear"]
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="container my-3">
+        <div class="d-flex justify-content-end">
+            <select class="form-select w-25" name="status" id="status" v-model="store.selectedStatus" @change="$emit('filter')">
+                <option value="">All</option>
+                <option :value="status" v-for="status in archetypeOptions">{{ status }}</option>
+            </select>
+        </div>
+    </div>
+</template>
+
